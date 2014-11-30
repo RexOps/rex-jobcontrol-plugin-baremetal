1893c6aadaf6eee21550902e9fe3da9a7b23d4fe
Jan <krimdomu@googlemail.com>
2014-11-30 12:43:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Baremetal Plugin for Rex JobControl
+
+
+This is a docker plugin for Rex JobControl to provision Baremetal machines.
+
+
+## INSTALLATION
+
+Currently this plugin is in early development status. So you have to clone the git repository.
+
+```
+git clone https://github.com/RexOps/rex-jobcontrol-plugin-baremetal.git
+```
+
+Then you can load the plugin from the jobcontrol configuration file.
+
+```
+{
+  plugins => [
+    'Rex::JobControl::Provision::Baremetal',
+  ],
+}
+```
+
+Before the restart of Rex JobControl you have to update the PERL5LIB env variable to point to the plugin repository:
+
+```
+export PERL5LIB=/path/to/rex-jobcontrol-plugin-baremetal/lib:$PERL5LIB
+```
---
+++ lib/Rex/JobControl/Provision/Baremetal.pm
@@ -0,0 +1,66 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+#
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+
+use strict;
+use warnings;
+
+package Rex::JobControl::Provision::Baremetal;
+
+use Moo;
+use YAML;
+use namespace::clean;
+use Rex::JobControl::Provision;
+use Data::Dumper;
+
+require Rex::Commands;
+use Rex::Commands::Virtualization;
+
+with 'Rex::JobControl::Provision::Base', 'Rex::JobControl::Plugin';
+
+Rex::JobControl::Provision->register_type('kvm');
+
+has host      => ( is => 'ro' );
+has name      => ( is => 'ro' );
+
+sub create {
+  my ($self) = @_;
+  $self->project->app->log->debug(
+    "Creating a baremetal machine. (stub)");
+}
+
+sub remove {
+  my ($self) = @_;
+
+  $self->project->app->log->debug(
+    "Removing a baremetal machine. (stub)");
+}
+
+sub get_data {
+  my ($self) = @_;
+  return {};
+}
+
+sub get_hosts {
+  my ($self) = @_;
+  return $self->project->get_nodes(
+    sub {
+      my ($file) = @_;
+      $self->project->app->log->debug(
+        "Reading $file to see if it is a Baremetal host.");
+      my $ref = YAML::LoadFile($file);
+      if ( exists $ref->{type}
+        && $ref->{type} eq "baremetal" )
+      {
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    }
+  );
+}
+
+1;
